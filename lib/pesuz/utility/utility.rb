f8ea317b8255288ca5868e32a44c8d84805dda90
andela-oesho <susan.esho@andela.com>
2016-05-28 09:54:05 +0100

    feat(): add dependencies file
    add snakecase method
    add pluralize
    add constantize
    add autoloading classes
    
    [ #120480239]

--- lib/pesuz/utility/utility.rb
+++ lib/pesuz/utility/utility.rb
@@ -7,4 +7,27 @@ class String
     downcase!
     self
   end
+
+  def camelize
+    return self if self !~ /_/ && self =~/[A-Z]+.*/
+    split('_').map{ |str| str.capitalize }.join
+  end
+
+  def constantize
+    Object.const_get(self)
+  end
+
+  def pluralize
+    gsub!(/([^aeiouy]|qu)y$/i, '1ies')
+    gsub!(/(ss|z|ch|sh|x)$/i, '1es')
+    gsub!(/(is)$/i, 'es')
+    gsub!(/(f|fe)$/i, 'ves')
+    gsub!(/(ex|ix)$/i, 'ices')
+    gsub!(/(a)$/i, 'ae')
+    gsub!(/(um|on)$/i, 'a')
+    gsub!(/(us)$/i, 'i')
+    gsub!(/(eau)$/i, 'eaux')
+    gsub!(/([^saeix])$/i, '1s')
+    self
+  end
 end
