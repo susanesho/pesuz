6df16c50b2e79e42d020a402a9b290a4e6d75c3d
andela-oesho <susan.esho@andela.com>
2016-05-30 17:44:17 +0100

    feat(): create route class
    create route class
    
    [ #120519187]

--- lib/pesuz.rb
+++ lib/pesuz.rb
@@ -1,26 +1,24 @@
-require "pesuz/version"
-require "pesuz/utility/utility"
-require "pesuz/routing/router"
+require 'pesuz/version'
+require 'pesuz/utility/utility'
+require 'pesuz/routing/router'
 
 module Pesuz
   class Application
     def call(env)
-      if env["PATH_INFO"] == "/"
-        return [ 302, { "location" => "pages"}, [] ]
-      end
+      return [302, { 'location' => 'pages' }, []] if env['PATH_INFO'] == '/'
       @rack_reqst = Rack::Request.new(env)
       path = @rack_reqst.path_info
       request_method = @rack_reqst.request_method.downcase
-      return [500, {}, []] if path == "/favicon.ico"
+      return [500, {}, []] if path == '/favicon.ico'
       controller, action = get_controller_and_action_for(path, request_method)
       response = controller.new.send(action)
-      [200, {"Content-Type" => "text/html"}, [response]]
+      [200, { 'Content-Type' => 'text/html' }, [response]]
     end
 
     def get_controller_and_action_for(path, verb)
-      _, controller, action, others = path.split("/", 4)
+      _, controller, action, others = path.split('/', 4)
       require "#{controller.downcase}_controller.rb"
-      controller = Object.const_get(controller.capitalize! + "Controller")
+      controller = Object.const_get(controller.capitalize! + 'Controller')
       action = action.nil? ? verb : "#{verb}_#{action}"
       [controller, action]
     end
