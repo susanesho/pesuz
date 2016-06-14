0fa9655efe8b602fbab04589f8f93a69a7225d18
andela-oesho <susan.esho@andela.com>
2016-06-14 19:34:40 +0100

    ll

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,16 +2,10 @@ require "coveralls"
 require "pesuz"
 require "rack"
 require "rspec"
-# require_relative "../spec/integration/suzsnam/config/application.rb"
 
 Coveralls.wear!
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 $LOAD_PATH.unshift File.expand_path("../../spec", __FILE__)
-
-module Suzsnam
-  class Application < Pesuz::Application
-  end
-end
 
 RSpec.shared_context type: :feature do
   require "capybara/rspec"
